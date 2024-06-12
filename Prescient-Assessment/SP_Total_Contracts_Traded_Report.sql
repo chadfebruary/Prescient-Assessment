736e23df0357b9ffe27725fa5202f6e012d32ab5
Chad February <chad@codeforte.com>
2024-06-12 15:23:56 +0200

    Completed stored procedure.

--- Prescient-Assessment/SP_Total_Contracts_Traded_Report.sql
+++ Prescient-Assessment/SP_Total_Contracts_Traded_Report.sql
@@ -1,13 +1,25 @@
-create proc SP_Total_Contracts_Traded_Report
-	@DateFrom date,
-	@DateTo date
-as
-begin
-	select
+CREATE PROCEDURE SP_Total_Contracts_Traded_Report
+	@DateFrom DATE,
+	@DateTo DATE
+AS
+BEGIN
+	DECLARE @DailyTotals TABLE(FileDate DATE, Total FLOAT)
+	INSERT INTO @DailyTotals
+	SELECT
 		FileDate,
-		[Contract],
-		ContractsTraded,
-		'' as [%OfTotalContractsTraded]
-	from dbo.DailyMTM
-	where filedate between @DateFrom and @DateTo
-end
+		SUM(ContractsTraded)
+	FROM dbo.DailyMtm
+	WHERE FileDate between @DateFrom and @DateTo and ContractsTraded > 0
+	GROUP BY filedate
+
+	SELECT
+		FORMAT(mtm.FileDate, 'MM/dd/yyyy') as [File Date],
+		mtm.[Contract] as [Contract],
+		SUM(mtm.ContractsTraded) as [Contracts Traded],
+		CAST((SUM(mtm.ContractsTraded) / MAX(dt.Total)) * 100 AS DECIMAL(10,8)) AS [% Of Total Contracts Traded]
+	FROM dbo.DailyMTM mtm
+	INNER JOIN @DailyTotals dt ON dt.FileDate = mtm.FileDate
+	WHERE mtm.FileDate between @DateFrom and @DateTo and mtm.ContractsTraded > 0
+	GROUP BY mtm.FileDate, mtm.[Contract]
+	ORDER BY mtm.FileDate, mtm.[Contract]
+END
